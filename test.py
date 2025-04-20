5564c98cb5421f19a66b92e57e47d4e5b6085d58
Rakesh Padhi <rakesh.rinku109@gmail.com>
2025-04-20 18:57:37 +0530

    init commit

---
+++ test.py
@@ -0,0 +1,8 @@
+def find_first_tenevennumbers():
+  count = 0
+  for number in range(1,100):
+    if (number % 2 == 0 and count < 10):
+      print(number)
+      ++count
+
+find_first_tenevennumbers()
